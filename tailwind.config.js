39aabc3b4fd2e9ea96dc40a104e270dcb0541318
yaxyobekb <pubgn9642@gmail.com>
2024-12-03 20:52:32 +0500

    add site initial colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "black",
+        primary: { default: "#0085FF", strong: "#1A3EDD" },
+        neutral: { dark: "#333333" },
+        gray: { light: "#f5f5f5", medium: "#d1d5db" },
+      },
+
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
       },
     },
   },
