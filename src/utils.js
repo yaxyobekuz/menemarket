e3682017fdf4ea7b12f38bfc54c568cab715a8b0
yaxyobekb <pubgn9642@gmail.com>
2024-12-20 21:02:49 +0500

    add utility function to generate a random number within a specified range

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,7 @@ export const sendUserCallOrderToServer = (data) => {
   const { firstName, phoneNumber, description } = data;
   alert(`Ma'lumotlar: ${firstName} ${phoneNumber} ${description}`);
 };
+
+export const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
