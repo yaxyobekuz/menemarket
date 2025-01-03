4b7c08f893e51550551b547b5476ee2f30c4c30f
yaxyobekb <pubgn9642@gmail.com>
2025-01-03 16:29:51 +0500

    Add getPercentageBgColor utility function for dynamic background color based on percentage

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,15 @@ export const sendUserCallOrderToServer = (data) => {
 export const getRandomNumber = (min = 0, max = 1) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export const getPercentageBgColor = (percentage = 0) => {
+  if (percentage >= 50) {
+    return "bg-green-500";
+  } else if (percentage >= 25) {
+    return "bg-yellow-500";
+  } else if (percentage > 0) {
+    return "bg-red-500";
+  } else {
+    return "bg-black";
+  }
+};
