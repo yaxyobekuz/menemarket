09b8a959cb0f72e8e047448fce2297bab2d1a612
yaxyobekb <pubgn9642@gmail.com>
2025-01-05 14:55:28 +0500

    refactor StreamItem and Streams components for improved responsiveness and layout adjustments

--- src/components/StreamItem.jsx
+++ src/components/StreamItem.jsx
@@ -10,51 +10,53 @@ const StreamItem = ({ data = {} }) => {
   return (
     <li className="space-y-0.5">
       {/* Top */}
-      <div className="flex items-center justify-between gap-3.5 bg-white p-4 rounded-t-lg">
-        <h3 className="text-lg font-semibold truncate">
+      <div className="flex items-center justify-between gap-[1px] bg-white p-3.5 rounded-t-lg sm:p-4 sm:gap-2">
+        <h3 className="text-[15px] font-semibold truncate xs:text-base sm:text-lg">
           Mening oqimim #{getRandomNumber(0, 99)}
         </h3>
 
         <div
           className={`${getPercentageBgColor(
             percentage
-          )} rounded-full px-2 py-0.5 text-white text-sm`}
+          )} rounded-full px-2 py-0.5 text-white text-[13px] max-sm:leading-[18px] sm:text-sm`}
         >
           {percentage}%
         </div>
       </div>
 
       {/* Main */}
-      <div className="bg-white p-4 space-y-3.5">
+      <div className="flex flex-col justify-between h-[180px] bg-white px-3.5 py-3 sm:h-[196px] sm:p-4">
         {/* Product */}
         <div className="space-y-1">
-          <b className="font-medium text-[17px]">Mahsulot</b>
+          <b className="font-medium text-[15px] sm:text-[17px]">Mahsulot</b>
           <Link
             to={`/products/product/productId`}
-            className="line-clamp-2 text-neutral-500 transition-colors duration-200 hover:text-primary-default"
+            className="line-clamp-2 text-neutral-500 text-[15px] transition-colors duration-200 hover:text-primary-default sm:text-base"
           >
             {data.title}
           </Link>
         </div>
 
-        {/* Url */}
-        <div className="bg-gradient-gray py-2 px-3 rounded-lg">
-          <div className="overflow-hidden">
-            <span>menemarket.uz/oqim/</span>
-            {getRandomNumber(0, 999999)}
+        <div className="space-y-3.5">
+          {/* Url */}
+          <div className="bg-gradient-gray py-2 px-3 rounded-lg">
+            <div className="overflow-hidden text-sm sm:text-base">
+              <span>menemarket.uz/oqim/</span>
+              {getRandomNumber(0, 999999)}
+            </div>
           </div>
-        </div>
 
-        {/* Date & Time */}
-        <div className="flex items-center justify-end gap-3.5">
-          <span className="text-neutral-500 text-sm">12/08/2024</span>
-          <span className="text-neutral-500 text-sm">12:00</span>
+          {/* Date & Time */}
+          <div className="flex items-center justify-end gap-3.5">
+            <span className="text-neutral-500 text-sm">12/08/2024</span>
+            <span className="text-neutral-500 text-sm">12:00</span>
+          </div>
         </div>
       </div>
 
       {/* Sub */}
-      <div className="flex items-center justify-between gap-3.5 bg-white p-4 rounded-b-lg">
-        <button className="btn-primary w-full h-10 font-normal">
+      <div className="flex items-center justify-between gap-3.5 bg-white p-3.5 rounded-b-lg sm:p-4">
+        <button className="btn-primary w-full h-10 font-normal text-sm xs:text-base">
           Nusxa olish
         </button>
       </div>
--- src/pages/Streams.jsx
+++ src/pages/Streams.jsx
@@ -9,10 +9,10 @@ import StreamItem from "../components/StreamItem";
 const Streams = () => {
   return (
     <div className="w-full pt-3.5 pb-8">
-      <div className="container">
+      <div className="container max-sm:px-1 ">
         <div className="bg-gradient-gray rounded-xl">
           {/* Title */}
-          <div className="flex items-center h-[60px] px-3.5 sm:px-4">
+          <div className="flex items-center h-[60px] px-2.5 sm:px-4">
             <h1 className="text-2xl">
               <span>Oqimlar </span>
               <span className="inline-block font-medium text-neutral-400 scale-90">
@@ -25,7 +25,7 @@ const Streams = () => {
           <div className="h-0.5 w-full bg-white" />
 
           {/* Content */}
-          <ul className="grid grid-cols-2 gap-3.5 px-3.5 py-5 sm:px-4 sm:grid-cols-3 sm:gap-4 md:grid-cols-4 lg:grid-cols-4">
+          <ul className="grid grid-cols-1 gap-3.5 px-2.5 py-5 sm:px-4 xs:grid-cols-2 sm:gap-4 xl:grid-cols-3 2xl:grid-cols-4">
             {products.map((product, index) => (
               <StreamItem key={index} data={product} />
             ))}
