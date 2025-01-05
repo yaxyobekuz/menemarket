2eaba22169dab6b9419928fc7f6a77c543279279
yaxyobekb <pubgn9642@gmail.com>
2025-01-05 16:02:35 +0500

    add header and layout tabs to AdminLayout for improved navigation and structure

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,26 +1,51 @@
 import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+
+// Images
+import logoIcon from "../assets/images/icons/logo.svg";
 
 // Components
 import Sidebar from "../components/Sidebar";
+import AdminLayoutTabs from "../components/AdminLayoutTabs";
 
 const AdminLayout = () => {
   const isCollapseStorage = localStorage.getItem("isCollapse");
   const [isCollapse, setIsCollapse] = useState(isCollapseStorage === "true");
 
   return (
-    <div className="flex min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Sidebar isCollapse={isCollapse} setIsCollapse={setIsCollapse} />
 
+      {/* Header */}
+      <header className="flex items-center sticky top-0 inset-x-0 z-20   w-full h-16 bg-white border-b sm:hidden">
+        <div className="container">
+          {/* Logo */}
+          <Link to="/" className="flex items-center gap-1">
+            {/* logo icon */}
+            <img
+              width={90}
+              height={48}
+              src={logoIcon}
+              alt="Mene Market logo svg icon"
+              className="w-[75px] h-10 xs:w-[82.5px] xs:h-11 md:w-[90px] md:h-12"
+            />
+          </Link>
+        </div>
+      </header>
+
+      {/* Main */}
       <main
         className={`${
           isCollapse
             ? "sm:w-[calc(100%-96px)]"
             : "sm:w-[calc(100%-96px)] lg:w-[calc(100%-320px)]"
-        } max-sm:w-full`}
+        } max-sm:pb-16 max-sm:w-full`}
       >
         <Outlet />
       </main>
+
+      {/* Layout tabs */}
+      <AdminLayoutTabs />
     </div>
   );
 };
