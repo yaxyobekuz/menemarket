ab3b4c5e83e32747f2b9d8c3bf8658fa1967c060
yaxyobekb <pubgn9642@gmail.com>
2025-01-12 22:19:15 +0500

    add disabled and required props to FormInputWrapper component

--- src/components/FormInputWrapper.jsx
+++ src/components/FormInputWrapper.jsx
@@ -13,8 +13,10 @@ const FormInputWrapper = ({
   type = "text",
   name = "input",
   className = "",
+  disabled = false,
   placeholder = "",
   maxLength = 2048,
+  required = false,
   defaultValue = "",
   autoFocus = false,
   autoComplete = "off",
@@ -39,6 +41,7 @@ const FormInputWrapper = ({
           name={name}
           id={inputId}
           value={value}
+          disabled={disabled}
           maxLength={maxLength}
           placeholder={placeholder}
           autoComplete={autoComplete}
@@ -55,11 +58,31 @@ const FormInputWrapper = ({
           name={name}
           id={inputId}
           value={value}
+          disabled={disabled}
           autoFocus={autoFocus}
           maxLength={maxLength}
           className="h-11 px-3.5"
           placeholder={placeholder}
           mask="+998 (__) ___-__-__"
+          replacement={{ _: /\d/ }}
+          autoComplete={autoComplete}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+      );
+    }
+
+    if (as === "input" && type === "otp") {
+      return (
+        <InputMask
+          name={name}
+          id={inputId}
+          value={value}
+          disabled={disabled}
+          mask="_ _ _ _"
+          autoFocus={autoFocus}
+          maxLength={maxLength}
+          className="h-11 px-3.5"
+          placeholder={placeholder}
           replacement={{ _: /\d/ }}
           autoComplete={autoComplete}
           onChange={(e) => handleChange(e.target.value)}
@@ -74,6 +97,7 @@ const FormInputWrapper = ({
           name={name}
           id={inputId}
           value={value}
+          disabled={disabled}
           autoFocus={autoFocus}
           maxLength={maxLength}
           className="h-11 px-3.5"
@@ -92,6 +116,8 @@ const FormInputWrapper = ({
         name={name}
         id={inputId}
         value={value}
+        disabled={disabled}
+        required={required}
         autoFocus={autoFocus}
         maxLength={maxLength}
         className="h-11 px-3.5"
