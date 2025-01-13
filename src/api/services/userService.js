0bce97fa4db6e70acc517002b3bd3370485c455d
yaxyobekb <pubgn9642@gmail.com>
2025-01-13 19:19:28 +0500

    refactor userService to use consistent endpoint naming and simplify API calls

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -2,44 +2,44 @@
 import api from "../axiosConfig";
 
 // Endpoints
-import apiEndpoints from "../apiEndpoints";
+import endpoints from "../apiEndpoints";
 
 const userService = {
   createUser: async (data) => {
-    const { url, data: payload } = apiEndpoints.createUser(data);
     try {
-      const res = await api.post(url, payload);
-      return res;
+      return await api.post(endpoints.createUser, data);
     } catch (err) {
       throw err;
     }
   },
 
   loginUser: async (data) => {
-    const { url, data: payload } = apiEndpoints.loginUser(data);
     try {
-      const res = await api.post(url, payload);
-      return res;
+      return await api.post(endpoints.loginUser, data);
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  getUserData: async () => {
+    try {
+      return await api.get(endpoints.getUserData);
     } catch (err) {
       throw err;
     }
   },
 
   verifyOtp: async (data) => {
-    const { url, data: payload } = apiEndpoints.verifyOtp(data);
     try {
-      const res = await api.post(url, payload);
-      return res;
+      return await api.post(endpoints.verifyOtp, data);
     } catch (err) {
       throw err;
     }
   },
 
   resendOtp: async (data) => {
-    const { url, data: payload } = apiEndpoints.resendOtp(data);
     try {
-      const res = await api.post(url, payload);
-      return res;
+      return await api.post(endpoints.resendOtp, data);
     } catch (err) {
       throw err;
     }
