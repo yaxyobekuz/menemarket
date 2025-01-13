a3f160de02d19dc2d9a8b18ca90d9628a76ce5e3
yaxyobekb <pubgn9642@gmail.com>
2025-01-13 19:19:19 +0500

    refactor API endpoint definitions for improved readability and consistency

--- src/api/apiEndpoints.js
+++ src/api/apiEndpoints.js
@@ -1,18 +1,9 @@
 const apiEndpoints = {
-  createUser: (data) => {
-    return { url: "api/users", data };
-  },
-  loginUser: (data) => {
-    return { url: "login", data };
-  },
-
-  verifyOtp: (data) => {
-    return { url: "api/users/verifyOTP", data };
-  },
-
-  resendOtp: (data) => {
-    return { url: "api/users/resendOTP", data };
-  },
+  loginUser: "login",
+  createUser: "api/users",
+  getUserData: "profile",
+  verifyOtp: "api/users/verifyOTP",
+  resendOtp: "api/users/resendOTP",
 };
 
 export default apiEndpoints;
