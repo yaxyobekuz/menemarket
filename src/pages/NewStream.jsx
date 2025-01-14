8056ebd8fdad511a1e5ce02770339133154808dc
yaxyobekb <pubgn9642@gmail.com>
2025-01-15 00:01:36 +0500

    feat: implement product loading and error handling in NewStream component

--- src/pages/NewStream.jsx
+++ src/pages/NewStream.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 // Data
-import products from "../data/products";
 import categories from "../data/categories";
 
+// Images
+import DotsLoader from "../components/DotsLoader";
+import reloadIcon from "../assets/images/icons/reload.svg";
+
 // Components
-import ProductItem from "../components/ProductItem";
+import Icon from "../components/Icon";
+import productService from "../api/services/productService";
+import StreamProductItem from "../components/StreamProductItem";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { updateProducts } from "../store/features/productsSlice";
 
 const NewStream = () => {
+  const dispatch = useDispatch();
+  const [hasError, setHasError] = useState(false);
+  const [isLoadingProducts, setIsLoadingProducts] = useState(true);
+  const allProductsFromStore = useSelector((state) => state.products.data);
+  const [filteredProducts, setFilteredProducts] = useState(
+    allProductsFromStore || []
+  );
+
+  const loadProducts = () => {
+    setHasError(false);
+    setIsLoadingProducts(true);
+
+    productService
+      .getProducts()
+      .then((products) => {
+        setFilteredProducts(products);
+        dispatch(updateProducts(products));
+      })
+      .catch(() => setHasError(true))
+      .finally(() => setIsLoadingProducts(false));
+  };
+
+  useEffect(() => {
+    if (allProductsFromStore?.length === 0) {
+      loadProducts();
+    } else {
+      setTimeout(() => setIsLoadingProducts(false), 500);
+    }
+  }, []);
+
   return (
     <div className="w-full pt-3.5 pb-8">
       <div className="container">
@@ -49,12 +88,38 @@ const NewStream = () => {
           </ul>
         </nav>
 
-        {/* Section content */}
-        <ul className="grid grid-cols-2 gap-x-3.5 gap-y-6 sm:grid-cols-3 sm:gap-x-4 md:gap-x-5 md:gap-y-8 xl:grid-cols-4 2xl:grid-cols-5">
-          {products.map((product, index) => (
-            <ProductItem key={index} data={product} />
-          ))}
-        </ul>
+        {/* Products */}
+        {!hasError &&
+        filteredProducts &&
+        !isLoadingProducts &&
+        filteredProducts?.length > 0 ? (
+          <ul className="grid grid-cols-2 gap-x-3.5 gap-y-6 sm:grid-cols-3 sm:gap-x-4 md:gap-x-5 md:gap-y-8 xl:grid-cols-4 2xl:grid-cols-5">
+            {filteredProducts.map((product) => (
+              <StreamProductItem key={product._id} data={product} />
+            ))}
+          </ul>
+        ) : null}
+
+        {/* Loading animation */}
+        {!hasError && isLoadingProducts && (
+          <div className="py-20">
+            <DotsLoader color="#0085FF" />
+          </div>
+        )}
+
+        {/* Reload button */}
+        {hasError && !isLoadingProducts && (
+          <div className="flex justify-center py-16">
+            <button
+              title="Reload"
+              className="p-1.5"
+              aria-label="Reload"
+              onClick={loadProducts}
+            >
+              <Icon src={reloadIcon} alt="Reload icon" />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
