133ed1b0626c669a951cf2a821f2ffbbdf09e334
yaxyobekb <pubgn9642@gmail.com>
2025-01-15 21:40:58 +0500

    add CreateStreamModalContent component for stream creation form

---
+++ src/components/CreateStreamModalContent.jsx
@@ -0,0 +1,42 @@
+import React, { useCallback } from "react";
+
+// Utils
+import { getRandomNumber } from "../utils";
+
+// Redux
+import { useSelector } from "react-redux";
+
+// Components
+import FormInputWrapper from "./FormInputWrapper";
+
+const CreateStreamModalContent = ({ updateFormData = () => {} }) => {
+  const { product } = useSelector((state) => state.modal.data);
+
+  // Update form data based on input changes
+  const handleInputChange = (field, value) => {
+    updateFormData((prevData) => ({
+      ...prevData,
+      [field]: value,
+    }));
+  };
+
+  return (
+    <div className="space-y-4 pb-4">
+      {/* First name */}
+      <FormInputWrapper
+        maxLength="72"
+        name="streamName"
+        label="Oqim nomi *"
+        placeholder={`Oqim #${getRandomNumber(0, 999)}`}
+        onChange={(value) => handleInputChange("streamName", value)}
+      />
+
+      <div className="space-y-2">
+        <b className="inline-block font-semibold">Mahsulot</b>
+        <p className="line-clamp-5">{product.title}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CreateStreamModalContent;
