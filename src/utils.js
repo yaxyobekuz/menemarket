2a7fbb70f59f425a9e58058b53b305c14e6b08d2
yaxyobekb <pubgn9642@gmail.com>
2025-01-15 21:37:23 +0500

    feat: add date and time formatting utilities to utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,3 @@
-export const sendUserCallOrderToServer = (data) => {
-  const { firstName, phoneNumber, description } = data;
-  alert(`Ma'lumotlar: ${firstName} ${phoneNumber} ${description}`);
-};
-
 export const getRandomNumber = (min = 0, max = 1) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -17,4 +12,25 @@ export const getPercentageBgColor = (percentage = 0) => {
   } else {
     return "bg-black";
   }
+};
+
+// Format date
+export const formatDate = (input) => {
+  const date = new Date(input);
+
+  const year = date.getFullYear();
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+
+  return `${day}/${month}/${year}`;
+};
+
+// Format time
+export const formatTime = (input) => {
+  const date = new Date(input);
+
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${hours}:${minutes}`;
 };
