7649cb427b1b6e86478bf6926944f8951a4b76e0
yaxyobekb <pubgn9642@gmail.com>
2025-01-15 21:38:15 +0500

    feat: add streams slice for managing stream data in Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 // Features
 import userSlice from "./features/userSlice";
 import modalSlice from "./features/modalSlice";
+import streamsSlice from "./features/streamsSlice";
 import productsSlice from "./features/productsSlice";
 
 export default configureStore({
   reducer: {
     user: userSlice,
     modal: modalSlice,
+    streams: streamsSlice,
     products: productsSlice,
   },
 });
