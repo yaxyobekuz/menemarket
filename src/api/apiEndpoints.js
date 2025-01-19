57662ab991b4af68f7d037e7bde891c6d2ef2ab2
yaxyobekb <pubgn9642@gmail.com>
2025-01-19 20:10:32 +0500

    feat: add new API endpoints for logout, news retrieval, and order creation

--- src/api/apiEndpoints.js
+++ src/api/apiEndpoints.js
@@ -1,4 +1,5 @@
 const apiEndpoints = {
+  logout: "logout",
   loginUser: "login",
   createUser: "api/users",
   getUserData: "profile",
@@ -6,9 +7,15 @@ const apiEndpoints = {
   resendOtp: "api/users/resendOTP",
 
   getProducts: "api/products",
+  getProduct: (id) => `api/products/${id}`,
+
+  getNews: "api/news",
+  getNewsById: (id) => `api/news/${id}`,
 
   getStreams: "api/oqim",
   createStream: (id) => `api/oqim/${id}`,
+
+  createOrder: (id) => `api/orders/${id}`,
 };
 
 export default apiEndpoints;
