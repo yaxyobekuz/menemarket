9d107f7696a52f08e75e77d92cb2e5a06140fa96
yaxyobekb <pubgn9642@gmail.com>
2025-01-19 20:15:01 +0500

    add: create Success component to display success message

---
+++ src/pages/Success.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Success = () => {
+  return (
+    <div className="container pt-8">
+      So'rovingiz muvaffaqiyatli qabul qilindi!
+    </div>
+  );
+};
+
+export default Success;
