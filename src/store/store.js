bcd42e825689a735c8b092d9abc482f2e3ded39f
yaxyobekb <pubgn9642@gmail.com>
2025-01-19 20:09:47 +0500

    add newsSlice for managing news data in the Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // Features
 import userSlice from "./features/userSlice";
+import newsSlice from "./features/newsSlice";
 import modalSlice from "./features/modalSlice";
 import streamsSlice from "./features/streamsSlice";
 import productsSlice from "./features/productsSlice";
@@ -9,6 +10,7 @@ import productsSlice from "./features/productsSlice";
 export default configureStore({
   reducer: {
     user: userSlice,
+    news: newsSlice,
     modal: modalSlice,
     streams: streamsSlice,
     products: productsSlice,
