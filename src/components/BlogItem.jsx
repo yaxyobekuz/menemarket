7ecefac65f9882a744f2abafa71e5337a13b2881
yaxyobekb <pubgn9642@gmail.com>
2025-01-20 21:13:46 +0500

    feat: update BlogItem component to improve data handling and default values

--- src/components/BlogItem.jsx
+++ src/components/BlogItem.jsx
@@ -2,25 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const BlogItem = ({ data = {} }) => {
+  const { banner: image, _id: id, desc: description, title } = data || {};
+
   return (
     <li className="w-full">
-      <Link
-        className="w-full group space-y-1.5"
-        to={`/blog/${encodeURIComponent(data.title)}`}
-      >
+      <Link to={`/blogs/blog/${id}`} className="w-full group space-y-1.5">
+        {/* Image */}
         <img
+          src={image}
           loading="lazy"
-          alt="News image"
-          src={data.image}
+          alt="Blog image"
           className="w-full h-auto aspect-[5/3] object-cover bg-gray-light rounded-xl"
         />
 
+        {/* Title */}
         <h3 className="line-clamp-3 font-medium transition-colors duration-200 group-hover:text-primary-default sm:text-lg">
-          {data.title}
+          {title || "Yangilik sarlavhasi"}
         </h3>
 
+        {/* Description */}
         <p className="line-clamp-2 text-neutral-500">
-          {data.description?.slice(0, 144)}
+          {description?.slice(0, 144) || "Yangilik haqida"}
         </p>
       </Link>
     </li>
