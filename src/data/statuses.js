b33897018cb75b8a4c4a91719caf12c4ca0c546a
yaxyobekb <pubgn9642@gmail.com>
2025-02-21 08:28:45 +0500

    feat: add status management module

---
+++ src/data/statuses.js
@@ -0,0 +1,51 @@
+import timeIcon from "../assets/images/icons/pending-gradient.svg";
+import crossIcon from "../assets/images/icons/cross-gradient.svg";
+import successIcon from "../assets/images/icons/success-gradient.svg";
+
+const statuses = [
+  {
+    label: "Kutilmoqda",
+    value: "pending",
+    color: "#eab308",
+    icon: timeIcon,
+  },
+  {
+    label: "Bekor qilingan",
+    value: "canceled",
+    color: "red",
+    icon: crossIcon,
+  },
+  {
+    label: "Qaytarilgan",
+    value: "rejected",
+    color: "red",
+    icon: crossIcon,
+  },
+  {
+    label: "Muvaffaqiyatli",
+    value: "success",
+    color: "#27d627",
+    icon: successIcon,
+  },
+  {
+    label: "Noma'lum",
+    value: "unknown",
+    color: "#333333",
+    icon: successIcon,
+  },
+];
+
+export const getStatusByValue = (value) => {
+  const defaultStatus = {
+    label: "Noma'lum",
+    value: "unknown",
+    color: "#333333",
+  };
+
+  return (
+    statuses.find((status) => status.value === value.toLowerCase()) ||
+    defaultStatus
+  );
+};
+
+export default statuses;
