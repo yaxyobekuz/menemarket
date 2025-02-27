221e0e82ca4be25a15e9a4d02e67e72608e70c98
yaxyobekb <pubgn9642@gmail.com>
2025-02-27 10:35:04 +0500

    add donate modal functionality to DonateBox component

--- src/pages/DonateBox.jsx
+++ src/pages/DonateBox.jsx
@@ -3,11 +3,32 @@ import React from "react";
 // Utils
 import { getRandomNumber } from "../utils";
 
+// Redux
+import { useDispatch } from "react-redux";
+import { updateModal } from "@/store/features/modalSlice";
+
 // Components
 import TransactionItem from "../components/TransactionItem";
 import AdminPagesHeader from "../components/AdminPagesHeader";
 
 const DonateBox = () => {
+  const dispatch = useDispatch();
+
+  const handleOpenDonateModal = () => {
+    dispatch(
+      updateModal({
+        data: null,
+        isOpen: true,
+        name: "donate",
+        title: "Ehson qilish",
+        buttons: {
+          primary: { label: "Ehson qilish" },
+          secondary: { label: "Bekor qilish" },
+        },
+      })
+    );
+  };
+
   return (
     <div className="w-full pt-3.5 pb-8 space-y-4">
       {/* Header */}
@@ -17,10 +38,21 @@ const DonateBox = () => {
       <div className="container">
         <div className="bg-gradient-gray rounded-xl">
           {/* Total balance */}
-          <div className="flex items-center justify-center h-[62px] border-b-2 border-white">
+          <div className="flex items-center justify-between h-[62px] pl-5 pr-2.5 border-b-2 border-white">
             <b className="block text-center text-lg font-normal truncate sm:text-xl">
               {getRandomNumber(0, 99999999).toLocaleString()} so'm
             </b>
+
+            {/* Button */}
+            <button
+              onClick={handleOpenDonateModal}
+              className="btn-primary h-10 px-5 rounded-full max-xs:font-normal"
+            >
+              <span className="hidden xs:inline">Ehson qilish</span>
+              <span className="text-2xl font-normal leading-none pb-0.5">
+                +
+              </span>
+            </button>
           </div>
 
           {/* Transactions */}
