1be3054f584c533adfd0c3e1c9ecb5f7f1511445
yaxyobekb <pubgn9642@gmail.com>
2025-03-10 20:07:56 +0500

    Add orders slice to manage order data in Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ import userSlice from "./features/userSlice";
 import newsSlice from "./features/newsSlice";
 import blogSlice from "./features/blogSlice";
 import modalSlice from "./features/modalSlice";
+import ordersSlice from "./features/ordersSlice";
 import donateSlice from "./features/donateSlice";
 import streamsSlice from "./features/streamsSlice";
 import productsSlice from "./features/productsSlice";
@@ -18,6 +19,7 @@ export default configureStore({
     blogs: blogSlice,
     modal: modalSlice,
     donate: donateSlice,
+    orders: ordersSlice,
     streams: streamsSlice,
     products: productsSlice,
     homeProducts: homeProductsSlice,
