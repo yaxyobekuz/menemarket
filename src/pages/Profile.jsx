023ebaab951d81797564cd9aa17bd8cd761ed2a5
yaxyobekb <pubgn9642@gmail.com>
2025-03-12 23:39:12 +0500

    Add password management section to Profile page and include password icon

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -24,6 +24,7 @@ import linkIcon from "../assets/images/icons/link.svg";
 import copyIcon from "../assets/images/icons/copy.svg";
 import emailIcon from "../assets/images/icons/email-gradient.svg";
 import telegramLogo from "../assets/images/others/telegram-logo.png";
+import passwordIcon from "../assets/images/icons/password-gradient.svg";
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -52,7 +53,7 @@ const Profile = () => {
 
   return (
     <div className="w-full pt-3.5 pb-8">
-      <div className="container space-y-3">
+      <div className="container space-y-4">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
           {/* Profile */}
           <div className="flex items-center justify-between gap-1.5 bg-gradient-gray p-3.5 rounded-xl md:col-span-2 sm:p-4 lg:px-4 lg:p-5">
@@ -125,30 +126,40 @@ const Profile = () => {
             </Link>
           </section>
 
-          {/* Id */}
-          <div className="flex flex-col justify-center gap-1.5 bg-gradient-gray p-3.5 rounded-xl md:col-span-2 sm:p-4 lg:p-5 2xl:col-span-1">
-            <b className="text-lg font-semibold md:text-xl">ID raqamingiz</b>
-
-            <CopyButton
-              text={_id || "mavjud-emas"}
-              notificationText="ID raqamdan nusxa olindi"
-              className="flex items-center justify-between text-start disabled:opacity-50"
-            >
-              <span className="text-neutral-400 truncate">
-                {_id || "mavjud-emas"}
-              </span>
-
+          {/* Password */}
+          <section className="flex items-center justify-between gap-1.5 bg-gradient-gray p-3.5 rounded-xl md:col-span-2 sm:p-4 lg:p-5">
+            <div className="flex items-center gap-3 min-w-0 2xl:gap-4">
               <Icon
                 size={64}
-                src={copyIcon}
-                alt="Copy icon"
-                className="size-6 shrink-0"
+                src={passwordIcon}
+                alt="Password icon"
+                className="size-12 rounded-full md:size-14 lg:size-16"
               />
-            </CopyButton>
-          </div>
+
+              {/* Details */}
+              <div className="overflow-hidden space-y-1">
+                <h2 className="text-lg font-semibold truncate md:text-xl lg:text-2xl">
+                  Parol
+                </h2>
+                <p className="text-neutral-400 truncate">Parolni boshqarish</p>
+              </div>
+            </div>
+
+            <Link
+              aria-label="Edit profile"
+              to="/admin/profile/edit-password"
+              className="btn shrink-0 size-10 bg-white rounded-full sm:size-12"
+            >
+              <Icon
+                src={editIcon}
+                alt="Edit icon"
+                className="size-[21px] xs:size-6"
+              />
+            </Link>
+          </section>
 
           {/* Telegram account */}
-          <section className="flex items-center justify-between gap-1.5 bg-gradient-gray p-3.5 rounded-xl md:col-span-2 sm:p-4 lg:p-5 2xl:col-span-3">
+          <section className="flex items-center justify-between gap-1.5 bg-gradient-gray p-3.5 rounded-xl md:col-span-2 sm:p-4 lg:p-5">
             <div className="flex items-center gap-3 min-w-0 2xl:gap-4">
               <Icon
                 size={64}
@@ -160,11 +171,11 @@ const Profile = () => {
               {/* Details */}
               <div className="overflow-hidden space-y-1">
                 <h2 className="text-lg font-semibold truncate md:text-xl lg:text-2xl">
-                  {name || "Foydalanuvchi"}
+                  Telegram bot
                 </h2>
 
                 <p className="text-neutral-400 truncate">
-                  Tg botga bog'langan akkaunt
+                  Tg botga akkauntni bog'lash
                 </p>
               </div>
             </div>
@@ -182,6 +193,29 @@ const Profile = () => {
           </section>
         </div>
 
+        {/* Id */}
+        <div className="flex flex-col justify-center gap-1.5 bg-gradient-gray p-3.5 rounded-xl sm:p-4 lg:p-5">
+          <b className="text-lg font-semibold md:text-xl">ID raqamingiz</b>
+
+          <CopyButton
+            text={_id || "mavjud-emas"}
+            notificationText="ID raqamdan nusxa olindi"
+            className="flex items-center justify-between text-start disabled:opacity-50"
+          >
+            <span className="text-neutral-400 truncate">
+              {_id || "mavjud-emas"}
+            </span>
+
+            <Icon
+              size={64}
+              src={copyIcon}
+              alt="Copy icon"
+              className="size-6 shrink-0"
+            />
+          </CopyButton>
+        </div>
+
+        {/* Logout */}
         <button
           onClick={handleLogout}
           className="px-2.5 text-primary-default text-[17px] font-medium underline underline-offset-2 sm:text-lg"
